79ce1c1ac026d20a6b1a3a8b525711806d732fc3
Justin Chang <justin.chang@goldenhippo.com>
2019-03-06 17:22:22 -0800

    first commit

---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="//player.ooyala.com/static/v4/production/latest/core.min.js"></script>
+	<script src="//player.ooyala.com/static/v4/production/latest/video-plugin/main_html5.min.js"></script>
+	<script src="//player.ooyala.com/static/v4/production/latest/skin-plugin/html5-skin.min.js"></script>
+	<link rel="stylesheet" href="//player.ooyala.com/static/v4/production/latest/skin-plugin/html5-skin.min.css" />
+    <link href="./normalize.css" rel="stylesheet" type="text/css">
+    <link href="./style.css" rel="stylesheet" type="text/css">
+    <link href="./images/GothamBook.otf" rel='stylesheet' type='text/css'>
+    <link href="./images/GothamMedium.otf" rel='stylesheet' type='text/css'>
+    <title>Beverly Hills MD</title>
+</head>
+<body class="body ">
+    <header class="header center">
+        <img class="header_logo" src="./images/bhmd_r-2@3x.png">
+    </header>
+    <div class="line"></div>
+    <div class="center">
+        <img class="as_seen_on" src="./images/logos@3x.png">
+    </div>
+    <div class="line"></div>
+    <div class="center">
+        <h1 class="caption">CARE FOR YOUR SKIN FROM WITHIN</h1>
+    </div>
+    <div class="video">
+        		<!-- start OOYALA ADD -->
+		<!-- BHBL Advanced Dermal Renewal VSL 180320A -->
+		<div id="LimelightEmbeddedPlayerFlash"></div>
+		<script>
+			var playerParam = {
+				pcode: 'AzY2kyOoR4h6urJ83C-Otm97ooqx',
+				autoplay: false ,
+				debug:false,
+				playerBrandingId: '3416780e84604fe9a32a8e6b582ff5b1',
+				skin: {
+					config: 'js/bhbl_m.json'
+				}
+			};
+			OO.ready(function() {
+				// change the second parameter (embed code) to change the video 
+				window.pp = OO.Player.create('LimelightEmbeddedPlayerFlash', 'NsbnVxZTE6nhmPwNK0pcMm2iWkCJQ2iv', playerParam);
+			});
+			</script>
+			<!-- end OOYALA ADD -->
+        <img class="video_button" src="./images/video-icon@3x.png">
+    </div>
+    <div class="next_step center">
+        <p class="next_step_text">May take 10 seconds to load. Make sure your speakers are on.</p>
+        <button class="next_step_button"><span class="next_step_button_text">NEXT STEP &#9654</span></button>
+        <p class="next_step_in_this_video">IN THIS VIDEO</p>
+    </div>
+    <div class="bullets">
+
+        <ul class="bullets_ul">
+            <li class="bullets_li">How to Reduce the Appearance of Wrinkles and Saggy Skin Without Seeing a Doctor</li>
+            <li class="bullets_li">Answer to "What Is Causing My Skin To Age Quickly?"</li>
+            <li class="bullets_li">Breakthrough Study on Exceptional<br> Skin Agers</li>
+            <li class="bullets_li">How To Become an Exceptional<br> Skin Ager</li>
+        </ul>
+    </div>
+    <div >
+        <h1 class="caption">BEVERLY HILLS MD ADVANCED DERMAL RENEWAL</h1>        
+    </div>
+    <div class="natural">
+        <p><i>Natural Skin Formula</i></p>
+        <p>This revolutionary formula combines six skin nourishing elements to rejuvenate skin from the inside out.</p>
+    </div>
+    <div class="benefits">
+        <div class="benefits_points">
+            <img class="benefits_image" src="./images/img 1@3x.png">
+            <p class="benefits_text">Targets internal problems that cause visible aging</p>
+        </div>
+        <div class="benefits_points">
+            <img class="benefits_image" src="./images/img 2@3x.png">
+            <p class="benefits_text">Reduces the appearance of sagging or drooping</p>
+        </div>
+        <div class="benefits_points">
+            <img class="benefits_image" src="./images/img 3@3x.png">
+            <p class="benefits_text">Diminishes the appearance of wrinkles and dullness</p>
+        </div>
+        <div class="benefits_points">
+            <img class="benefits_image" src="./images/img 4@3x.png">
+            <p class="benefits_text">Protects and enhances skin elasticity</p>
+        </div>
+    </div>
+    <footer class="footer">
+        <p class="phone_number center"><img class="phone_icon" src="./images/phone@3x.png"> 1-877-828-5528 <br> <img class="phone_icon" src="./images/envelop@3x.png"> Click Here to Email Us</p>
+        <p class="footer_address center">Beverly Hills MD <br> 8383 Wilshire Blvd #800 <br> Beverly Hills, CA 90211 </p>
+        <p class="footer_disclaimer center">The information presented on this website is not intended as specific medical advice and is not a substitute for professional treatment or diagnosis. These statements have not been evaluated by the Food and Drug Administration. This product is not intended to diagnose, treat, cure, or prevent any disease.</p>
+        <p class="footer_copyright center">© Beverly Hills MD 2018. “Beverly Hills MD” is a registered trademark of Beverly Hills MD, LLC. All Rights Reserved. | PRIVACY POLICY | TERMS & CONDITIONS</p>
+    </footer>
+
+</body>
+</html>
